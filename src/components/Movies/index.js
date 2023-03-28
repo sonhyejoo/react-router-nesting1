6a4238082bb6a8f0af9d6b94ae0f8acff8bf48a8
Son Hyejoo <seongwoo.cho@gmail.com>
2023-03-28 17:18:58 +0000

    setup match url in movies

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,11 @@
-function Movies() {
+import { Route, useRouteMatch } from "react-router-dom";
+function Movies({ movies }) {
+  const match = useRouteMatch();
+  const url = match.url;
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Movies Component</h1>
+      <Route to="/"></Route>
     </div>
   );
 }
